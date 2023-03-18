06bb3403b5262783c452650a3f06dbe2a77f03c8
redanzo <66446416+redanzo@users.noreply.github.com>
2023-03-17 22:31:13 -0500

    FIX: google sign-in
    better code and actually redirects, but still fails rip

--- src/js/googleAuth.js
+++ src/js/googleAuth.js
@@ -1,68 +1,61 @@
-import { getAuth, signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDKA_JPDFYUqdZNPC_maYkpqCNUnukIrOQ",
-  authDomain: "yieldly-gdsc.firebaseapp.com",
-  projectId: "yieldly-gdsc",
-  storageBucket: "yieldly-gdsc.appspot.com",
-  messagingSenderId: "509287211839",
-  appId: "1:509287211839:web:209b7c2221f443ca9f298a"
+var firebaseConfig = {
+    apiKey: "AIzaSyDKA_JPDFYUqdZNPC_maYkpqCNUnukIrOQ",
+    authDomain: "yieldly-gdsc.firebaseapp.com",
+    projectId: "yieldly-gdsc",
+    storageBucket: "yieldly-gdsc.appspot.com",
+    messagingSenderId: "509287211839",
+    appId: "1:509287211839:web:209b7c2221f443ca9f298a"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+firebase.initializeApp(firebaseConfig);
 
-document.getElementById('google').addEventListener('click',  _ => {
-    // signInWithPopup(auth, provider)
-    // .then((result) => {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     const credential = GoogleAuthProvider.credentialFromResult(result);
-    //     const token = credential.accessToken;
-    //     // The signed-in user info.
-    //     const user = result.user;
-    //     // IdP data available using getAdditionalUserInfo(result)
-    //     // ...
-    //     window.location.href = 'profile.html'
-    // }).catch((error) => {
-    //     console.error(error);
-    //     // Handle Errors here.
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     const email = error.customData.email;
-    //     // The AuthCredential type that was used.
-    //     const credential = GoogleAuthProvider.credentialFromError(error);
-    //     // ...
-    // });
+// document.getElementById('google').addEventListener('click', _ => {
+//     var provider = new firebase.auth.GoogleAuthProvider();
 
+//     firebase.auth().signInWithPopup(provider)
+//         .then((result) => {
+//         // User signed in successfully
+//         window.location.href = 'profile.html';
+//         })
+//         .catch((error) => {
+//         // Handle errors
+//         console.error(error);
+//     });
+// });
 
-    signInWithRedirect(auth, provider);
-    getRedirectResult(auth)
-    .then((result) => {
-        // This gives you a Google Access Token. You can use it to access Google APIs.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
+document.getElementById('google').addEventListener('click', _ => {
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-        // The signed-in user info.
-        const user = result.user;
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-        window.location.href = 'profile.html'
-    }).catch((error) => {
-        console.log(error);
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult()
+    .then(result => {
+        window.location.href = 'profile.html';
+    })
+    .catch(error => {
+        console.error(error);
     });
-})
+});
 
-// window.location.href = 'profile.html'
-
-
+// document.getElementById('google').addEventListener('click', _ => {
+//     function googleSignIn() {
+//         const provider = new firebase.auth.GoogleAuthProvider();
+    
+//         // Open the Google sign-in page in a new tab
+//         window.open('about:blank', 'google_signin', 'width=600,height=700');
+//         firebase.auth().signInWithRedirect(provider);
+//     }
+    
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {
+//             window.location.href = 'profile.html';
+//         }
+//     });
+    
+//     firebase.auth().getRedirectResult()
+//     .then(result => {
+//         window.location.href = 'profile.html';
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+//});
