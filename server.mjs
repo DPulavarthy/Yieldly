ab1ad8f590c48a3202e098c13cbe7a5ff18a82f8
redanzo <66446416+redanzo@users.noreply.github.com>
2023-03-23 17:31:31 -0500

    Created Navbar / Updated images / Created rest of the pages' html/css files

--- server.mjs
+++ server.mjs
@@ -2,6 +2,6 @@ import express from 'express';
 
 express()
     .use(express.static('src'))
-    .get('/', (req, res) => res.sendFile('login.html', { root: './src/html' }))
-    .get('/:path', (req, res) => res.sendFile(`${req.params.path.replace(/\.html/g, '')}.html`, { root: './src/html' }))
+    .get('/', (req, res) => res.sendFile('index.html', { root: './src' }))
+    .get('/:path', (req, res) => res.sendFile(`${req.params.path.replace(/\.html/g, '')}.html`, { root: './src' }))
     .listen(35349, () => console.log('Listening on port 35349!'));
