caa2342a84e9b52ee7c121a56d8cf34f49489b4f
Kurasad <akowaidev@gmail.com>
2023-03-23 13:16:35 -0500

    Feat: Remove server + New URL

--- App.js
+++ App.js
@@ -1,2 +1,2 @@
 import { WebView } from 'react-native-webview';
-export default _ => <WebView source={{ uri: 'http://10.0.2.2:35349' }} />;
+export default _ => <WebView source={{ uri: 'https://yieldly.netlify.app' }} />;
--- server.mjs
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-express()
-    .use(express.static('src'))
-    .get('/', (req, res) => res.sendFile('login.html', { root: './src/html' }))
-    .get('/:path', (req, res) => res.sendFile(`${req.params.path.replace(/\.html/g, '')}.html`, { root: './src/html' }))
-    .listen(35349, () => console.log('Listening on port 35349!'));
