8fa9bdfd4ef1e8a0201d691519146724378c2fd6
redanzo <66446416+redanzo@users.noreply.github.com>
2023-03-27 12:27:57 -0500

    data.js changes

---
+++ src/js/data.js
@@ -0,0 +1,12 @@
+document.getElementById('calcBtn').addEventListener('click', _ => {
+    window.location.href = 'calc.html'
+});
+document.getElementById('marketBtn').addEventListener('click', _ => {
+    window.location.href = 'market.html'
+});
+document.getElementById('profileBtn').addEventListener('click', _ => {
+    window.location.href = 'profile.html'
+});
+
+// const userLocation = localStorage.getItem('userLocation');
+// document.getElementById('locationHeading').textContent = userLocation + ' Data';
