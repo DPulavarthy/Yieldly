ee06411f5cd9fcef84566f42cb479771c1b3be26
Kopuri <jek200000@utdallas.edu>
2023-03-27 17:18:03 -0500

    changed the layout of profile

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,20 +1,29 @@
-document.getElementById('logout').addEventListener('click', function() {
-    firebase.auth().signOut().then(function() {
+document.getElementById('logout').addEventListener('click', function () {
+    firebase.auth().signOut().then(function () {
         window.location.href = 'login.html#skip'
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.log(error);
     });
     window.location.href = 'index.html#skip'
 });
 
-document.getElementById('editBtn').addEventListener('click', function() {
-    const firstname = document.getElementById('#firstname'),
-    lastname = document.getElementById('#lastname'),
-    email = document.getElementById('#email'),
-    location = document.getElementById('#location');
+document.getElementById("savePfpEdit").style.display = "none";
+document.getElementById('firstnametxtbx').disabled = true;
+document.getElementById('lastnametxtbx').disabled = true;
+document.getElementById('emailtxtbx').disabled = true;
 
-    alert("testing");
+document.getElementById('editBtn').addEventListener('click', function () {
+    document.getElementById("savePfpEdit").style.display = "block";
+    document.getElementById('firstnametxtbx').disabled = false;
+    document.getElementById('lastnametxtbx').disabled = false;
+    document.getElementById('emailtxtbx').disabled = false;
+});
 
+document.getElementById('savePfpEdit').addEventListener('click', function () {    
+    document.getElementById("savePfpEdit").style.display = "none";
+    document.getElementById('firstnametxtbx').disabled = true;
+    document.getElementById('lastnametxtbx').disabled = true;
+    document.getElementById('emailtxtbx').disabled = true;
 });
 
 document.getElementById('dataBtn').addEventListener('click', _ => {
@@ -28,12 +37,12 @@ document.getElementById('marketBtn').addEventListener('click', _ => {
 });
 
 fetch('http://ip-api.com/json')
-            .then(response => response.json())
-            .then(data => {
-                const city = data.city;
-                const state = data.regionName;
-                document.getElementById('location').textContent = city + ", " + state;
-            })
-            .catch(error => {
-                console.error(error);
-            });
+    .then(response => response.json())
+    .then(data => {
+        const city = data.city;
+        const state = data.regionName;
+        document.getElementById('location').textContent = city + ", " + state;
+    })
+    .catch(error => {
+        console.error(error);
+    });
